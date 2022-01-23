7ca346e4eed994e7bc63e10492256a345bda3754
meera-ramesh19 <meeraramesh19@outlook.com>
2022-01-23 11:50:21 -0500

    more 8kyus

---
+++ 8kyus-folder/removefirstandlastchar.js
@@ -0,0 +1,20 @@
+Remove First and Last Character
+
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+FUNDAMENTALSBASIC LANGUAGE FEATURESSTRINGS
+
+
+
+
+
+
+
+function removeChar(str){
+    //You got this!
+     str=str.split('')
+     str.shift()+str.pop()
+    return str.join('')
+     
+   };
+   
---
+++ 8kyus-folder/removestringspaces.js
@@ -0,0 +1,18 @@
+
+
+
+Remove String Spaces
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+FUNDAMENTALSSTRINGSARRAYS
+
+
+function noSpace(x){
+    let newString=''
+    x=x.split(' ')
+     for(let i=0;i<x.length;i++){
+       if(x[i]!=='') newString+=x[i]
+     }
+    return x.join('')
+   }
