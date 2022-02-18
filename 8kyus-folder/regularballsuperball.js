e1af604b9c499b8d9ab7d59eb1872b59193319d6
meera-ramesh19 <meeraramesh19@outlook.com>
2022-02-18 09:13:54 -0500

    8kys added

---
+++ 8kyus-folder/regularballsuperball.js
@@ -0,0 +1,17 @@
+// Regular Ball Super Ball
+
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+// FUNDAMENTALSCLASSESBASIC LANGUAGE FEATURESOBJECT-ORIENTED PROGRAMMINGOBJECTS
+
+var Ball = function (ballType = "regular") {
+  // your code goes here\
+  this.ballType = ballType;
+};
---
+++ 8kyus-folder/vowelremover.js
@@ -0,0 +1,24 @@
+// Vowel remover
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+// don't worry about uppercase vowels
+// y is not considered a vowel for this kata
+// FUNDAMENTALSSTRINGSPARSINGALGORITHMS
+
+function shortcut(string) {
+  return string.split("").reduce((acc, cur) => {
+    return cur === "a" ||
+      cur === "e" ||
+      cur === "i" ||
+      cur === "o" ||
+      cur === "u"
+      ? (acc += "")
+      : (acc += cur);
+  }, "");
+}
