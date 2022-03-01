c0d7a5f386397ab8962ebd3ec3445be6c930c431
meera-ramesh19 <meeraramesh19@outlook.com>
2022-03-01 09:44:34 -0500

    8kyus added

---
+++ 8kyus-folder/convertbooleantostring.js
@@ -0,0 +1,12 @@
+// Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// FUNDAMENTALSBOOLEANSSTRINGSFUNCTIONSCONTROL FLOWBASIC LANGUAGE FEATURES
+
+function booleanToString(b) {
+  //your code here\
+  return String(b);
+}
